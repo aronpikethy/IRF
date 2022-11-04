6640a4664eb75a83eec869bd4484074b835daa8f
aronpikethy <pikethy.aron@virzrt.hu>
2022-11-04 19:44:17 +0100

    interface init

--- UserMaintenance/Factory/Entities/Ball.cs
+++ UserMaintenance/Factory/Entities/Ball.cs
@@ -5,33 +5,15 @@ using System.Windows.Forms;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using Factory.Abstractions;
 
 namespace Factory.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            this.AutoSize = false;
-            this.Width = 50;
-            this.Height = 50;
-
-            this.Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, this.Width, this.Height);
-        }
-
-        public void MoveBall()
-        {
-            this.Left++;
         }
     }
 }
