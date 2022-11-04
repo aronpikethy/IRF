60859d9a442f6b27378bb961cfc4631a3e72c86d
aronpikethy <pikethy.aron@virzrt.hu>
2022-11-04 19:20:25 +0100

    Ball

---
+++ UserMaintenance/Factory/Entities/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Factory.Entities
+{
+    public class Ball : Label
+    {
+        public Ball()
+        {
+            this.AutoSize = false;
+            this.Width = 50;
+            this.Height = 50;
+
+            this.Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, this.Width, this.Height);
+        }
+
+        public void MoveBall()
+        {
+            this.Left++;
+        }
+    }
+}
